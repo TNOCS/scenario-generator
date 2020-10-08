75fa65da24dd64eecba1f25b9b157f4a21006ad6
Rinze <rwbpost@outlook.com>
2020-10-08 09:54:39 +0200

    Add docs folder

--- packages/client/vue.config.js
+++ packages/client/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/dashboard/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/dashboard/' : '/',
+  outputDir: '../../docs'
 };
